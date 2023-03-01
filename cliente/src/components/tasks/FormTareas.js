03edb863021c93071cd45172997d8fc5072a522b
Jorge Osvaldo Redonda Rodríguez <jorgeosvaldorr94@gmail.com>
2023-03-01 01:52:32 -0300

    Revisar con la IA, para hacer funcional el boton de descripcion

--- cliente/src/components/tasks/FormTareas.js
+++ cliente/src/components/tasks/FormTareas.js
@@ -1,6 +1,11 @@
 import React, { useContext, useState, useEffect } from 'react';
 import proyectoContext from '../../context/proyectos/proyectoContext';
 import tareaContext from '../../context/tareas/tareaContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+
+// Importar el componente para la Descripcion
+import Descripcion from './Descripcion';
 
 const FormTareas = () => {
     
@@ -10,7 +15,153 @@ const FormTareas = () => {
 
     //obtener la funcion del context de tarea
     const tareasContext = useContext(tareaContext);
-    const { tareaseleccionada, errortarea, agregarTarea, validarTarea, obtenerTareas, actualizarTarea, limpiarTarea } = tareasContext; 
+    const { texto, tareaseleccionada, errortarea, agregarTarea, validarTarea, obtenerTareas, actualizarTarea, limpiarTarea } = tareasContext; 
+
+    // State del Formulario
+    const [ tarea, guardarTarea ] = useState({
+        nombre: ''
+    })
+
+    const [mostrarDescripcion, setMostrarDescripcion] = useState(false);
+
+    // Effect que detecta si hay una tarea seleccionada
+    useEffect( () => {
+        if(tareaseleccionada !== null) {
+            guardarTarea(tareaseleccionada);
+        } else {
+            guardarTarea({
+                nombre: ''
+            });
+        }
+    }, [tareaseleccionada]);
+
+    //extraer el nombre del proyecto
+    const { nombre } = tarea;
+
+    //Si no hay proyecto seleccionado
+    if (!proyecto) return null;
+
+    // Arry destructuring para extraer el proyecto actual
+    const [proyectoActual] = proyecto;
+
+    //// Leer los valores delformulario
+    const handleChange = e => {
+        guardarTarea({
+            ...tarea,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        //Validar
+        if(nombre.trim() === ''){
+            validarTarea();
+            return;
+        }
+
+        // Revisar si es edicion o nueva tarea
+        if(tareaseleccionada === null) {
+            // Pasar la validación, lo hice en la funcion agregarTarea(tarea), ahi volvi a poner el errortarea: false, para que este como su estado inicial 
+    
+            // Agregar la nueva tarea al state de tareas, tambien le agrego las propiedades que yo quiera
+            tarea.proyecto = proyectoActual._id;
+            agregarTarea(tarea);
+        } else {
+            // actualizar tarea existente
+            actualizarTarea(tarea);
+
+            //elimina tarea seleccionada del state
+            limpiarTarea();
+        };
+
+        //obtener y filtrar las tareas del proyecto actual
+        obtenerTareas(proyectoActual.id);
+
+        //reiniciar el form    
+        guardarTarea({
+            nombre: ''
+        });
+
+    }
+    
+    
+    return ( 
+        <div className='formulario'>
+            <form
+                onSubmit={onSubmit}
+            >
+                <div>
+                    {tareaseleccionada
+                        ?
+                        <h3 className='tarea-action'>Editando Tarea</h3>
+                        :
+                        <h3 className='tarea-action'>Crear nueva Tarea</h3>
+                    }                    
+                </div>
+                <div className='contenedor-imput'>
+                    <input
+                        type='text'
+                        className='input-text'
+                        placeholder='Nombre Tarea...'
+                        name='nombre'
+                        value={nombre}
+                        onChange={handleChange}
+                    />
+                </div>
+    
+                <div className='contenedor-imput'>
+                    <input
+                        type='submit'
+                        className='btn-primario-editar'
+                        value={tareaseleccionada 
+                                ?
+                                    "Editar" 
+                                :
+                                    "Agregar"
+                        }
+                    />
+                </div>
+    
+                <div className='contenedor-imput'>
+                    <button 
+                    type='button' 
+                    className='btn btn-desc' 
+                    onClick={() => setMostrarDescripcion(true)}
+                    >
+                        <FontAwesomeIcon icon={faEye} />
+                    </button>
+                </div>
+            </form>
+    
+            {errortarea 
+                ? 
+                    <p className='mensaje error'>El nombre de la tarea es obligatorio</p> 
+                : 
+                    null
+            }
+    
+        </div>
+    );
+}
+
+export default FormTareas;
+
+/*
+import React, { useContext, useState, useEffect } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
+
+const FormTareas = () => {
+    
+    // Extraer si un proyecto esta activo
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
+
+    //obtener la funcion del context de tarea
+    const tareasContext = useContext(tareaContext);
+    const { texto, tareaseleccionada, errortarea, agregarTarea, validarTarea, obtenerTareas, actualizarTarea, limpiarTarea } = tareasContext; 
 
     // Effect que detecta si hay una tarea seleccionada
     useEffect( () => {
@@ -129,3 +280,4 @@ const FormTareas = () => {
 }
  
 export default FormTareas;
+*/
