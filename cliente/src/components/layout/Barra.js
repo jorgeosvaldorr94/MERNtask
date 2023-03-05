24c902842e0041b119b4c972954542b0a8799a1c
Jorge Osvaldo Redonda Rodríguez <jorgeosvaldorr94@gmail.com>
2023-03-05 20:09:54 -0300

    Arreglado Cerrar Sesion, revisar eliminar Proyecto

--- cliente/src/components/layout/Barra.js
+++ cliente/src/components/layout/Barra.js
@@ -1,3 +1,47 @@
+
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Paso 1
+import AuthContext from '../../context/autenticacion/authContext';
+
+const Barra = () => {
+    const navigate = useNavigate(); // Paso 3
+
+    const authContext = useContext(AuthContext);
+    const { usuario, usuarioAutenticado, cerrarSesion } = authContext;
+
+    useEffect(() => {
+        usuarioAutenticado();
+        // eslint-disable-next-line
+    }, []);
+
+    const handleCerrarSesion = () => {
+        cerrarSesion();
+        navigate('/'); // Paso 4
+    };
+
+    return (
+        <header className='app-header'>
+            {usuario ? (
+                <p className='nombre-usuario'>
+                    Hola <span>{usuario.nombre}</span>
+                </p>
+            ) : null}
+
+            <nav className='nav-principal'>
+                <button
+                    className='btn btn-blank cerrar-sesion'
+                    onClick={() => handleCerrarSesion()} // Cambio en onClick
+                >
+                    Cerrar Sesión
+                </button>
+            </nav>
+        </header>
+    );
+};
+
+export default Barra;
+
+/*
 import React, { useContext, useEffect } from 'react';
 import AuthContext from '../../context/autenticacion/authContext';
 
@@ -34,3 +78,4 @@ const Barra = () => {
 }
 
 export default Barra;
+*/
